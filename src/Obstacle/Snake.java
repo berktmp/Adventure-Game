cbcdb40fc4df1882dafa208274292dc70ed772ff
Berk Tmp <130314590+berktmp@users.noreply.github.com>
2023-06-10 11:50:18 +0300

    Create Snake.java

---
+++ src/Obstacle/Snake.java
@@ -0,0 +1,9 @@
+package Obstacle;
+
+import Obstacle.Obstacle;
+
+public class Snake extends Obstacle {
+    public Snake() {
+        super(4, "Snake", 3 + (int) (Math.random() * 4), 12, 0);
+    }
+}
